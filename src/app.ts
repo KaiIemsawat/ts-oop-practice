3bbc531397afb1e8930c127b570483346cd4ea5d
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-08-28 22:13:29 -0400

    refactored and managed classes

--- src/app.ts
+++ src/app.ts
@@ -13,15 +13,24 @@ class Project {
     ) {}
 }
 
-/* ===== Project State Management class ===== */
-type Listener = (items: Project[]) => void;
+type Listener<T> = (items: T[]) => void;
 
-class ProjectState {
-    private listeners: Listener[] = [];
+/* ===== State class ===== */
+class State<T> {
+    protected listeners: Listener<T>[] = [];
+    addListener(listenerFn: Listener<T>) {
+        this.listeners.push(listenerFn);
+    }
+}
+
+/* ===== Project State Management class ===== */
+class ProjectState extends State<Project> {
     private projects: Project[] = [];
 
     private static instance: ProjectState;
-    private constructor() {}
+    private constructor() {
+        super();
+    }
 
     static getInstance() {
         if (this.instance) {
@@ -29,10 +38,6 @@ class ProjectState {
         }
         this.instance = new ProjectState();
         return this.instance;
-    }
-
-    addListener(listenerFn: Listener) {
-        this.listeners.push(listenerFn);
     }
 
     addProject(title: string, description: string, numOfPeople: number) {
