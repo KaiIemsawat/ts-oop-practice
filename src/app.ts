1ce675db6e78e0ec68e8889824fc8664a7a55558
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-08-31 09:57:52 -0400

    refactoring namespaces

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
 // 3 Forward slashes is a typescript special syntax. Not in js
 // add path to the file needed to import
-// ! Becareful using these. Need to check tsconfig.json for "module": "commonjs" to "module": "amd"
+// ! Be careful using these. Need to check tsconfig.json for "module": "commonjs" to "module": "amd"
 // * Also check <script src="dist/bundle.js" defer></script> in html
-///<reference path="models/drag-drop.ts"/>
-///<reference path="models/project.ts"/>
-///<reference path="state/project-state.ts"/>
-///<reference path="utils/validation.ts"/>
-///<reference path="decorators/autobind.ts"/>
 ///<reference path="components/project-input.ts"/>
 ///<reference path="components/project-list.ts"/>
 
